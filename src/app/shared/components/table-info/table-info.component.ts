a60bafc5a34c3e879cffbec77d73ec722f3659f2
Beyza <bbasaga06@gmail.com>
2022-08-24 17:37:33 +0300

    updated table-info
    Co-Authored-By: Esra Özdoğan <50195220+EsraOzdogan@users.noreply.github.com>
    Co-Authored-By: Esin Çağla KIRAL <68962573+esincaglakiral@users.noreply.github.com>

--- src/app/shared/components/table-info/table-info.component.ts
+++ src/app/shared/components/table-info/table-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table-info',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-info.component.css']
 })
 export class TableInfoComponent implements OnInit {
-
+  @Input() tableList!:any[];
   constructor() { }
 
   ngOnInit(): void {
