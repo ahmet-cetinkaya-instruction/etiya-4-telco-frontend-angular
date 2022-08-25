cfa9f50cf1228d5692a9748a2b5d84889aaaa0ab
mirzacckc <cicekcimirzacan@gmail.com>
2022-08-25 14:28:00 +0300

    add input directive
    Co-Authored-By: Duygu Uçgun <83750292+duygucgun@users.noreply.github.com>
    Co-Authored-By: ÖMER DEMİRKAN <63349869+omerdemiirkan@users.noreply.github.com>
    Co-Authored-By: Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>

---
+++ src/app/shared/directives/input.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[etiyaInput]'
+})
+export class InputDirective {
+  @Input() etiyaInput!:'white' | 'dark';
+
+  constructor(private renderer: Renderer2,private hostElement: ElementRef) {   
+  }
+
+  ngOnInit(){
+    this.renderer.addClass(this.hostElement.nativeElement, `e-input-${this.etiyaInput}`);
+  }
+}
