bd498d964359380a579f651ea994b047a0e0826c
mirzacckc <cicekcimirzacan@gmail.com>
2022-08-26 12:30:07 +0300

    add customer-billing-account-detail component
    Co-Authored-By: Duygu Uçgun <83750292+duygucgun@users.noreply.github.com>
    Co-Authored-By: ÖMER DEMİRKAN <63349869+omerdemiirkan@users.noreply.github.com>
    Co-Authored-By: Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>

---
+++ src/app/features/customers/pages/customer-billing-account-detail/customer-billing-account-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './customer-billing-account-detail.component.html',
+  styleUrls: ['./customer-billing-account-detail.component.css']
+})
+export class CustomerBillingAccountDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
