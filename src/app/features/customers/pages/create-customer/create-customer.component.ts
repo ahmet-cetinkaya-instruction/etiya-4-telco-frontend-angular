21771fdd9a5453cf2db56433704e0cafeddacd22
Sanchklyc <halitenes.kalayci@gmail.com>
2022-08-27 15:13:55 +0300

    "ngrx implementation"

--- src/app/features/customers/pages/create-customer/create-customer.component.ts
+++ src/app/features/customers/pages/create-customer/create-customer.component.ts
@@ -1,23 +1,45 @@
+import { map, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { Customer } from '../../models/customer';
+import { CustomersService } from '../../services/customer/customers.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './create-customer.component.html',
-  styleUrls: ['./create-customer.component.css']
+  styleUrls: ['./create-customer.component.css'],
 })
 export class CreateCustomerComponent implements OnInit {
-  profileForm!:FormGroup;
+  profileForm!: FormGroup;
+  createCustomerModel$!: Observable<Customer>;
 
-  constructor(private formBuilder:FormBuilder) { }
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private customerService: CustomersService,
+    private router: Router
+  ) {
+    this.createCustomerModel$ = this.customerService.customerToAddModel$;
+  }
 
   ngOnInit(): void {
     this.createFormUpdateCustomer();
   }
 
-  createFormUpdateCustomer(){
+  createFormUpdateCustomer() {
     this.profileForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
+      firstName: [
+        this.createCustomerModel$.pipe(
+          map(state => {
+            return state.firstName;
+          })
+        ),
+        Validators.required,
+      ],
       middleName: [''],
       lastName: ['', Validators.required],
       birthDate: ['', Validators.required],
@@ -26,5 +48,9 @@ export class CreateCustomerComponent implements OnInit {
       motherName: [''],
       nationalityId: ['', Validators.required],
     });
+  }
+  goNextPage() {
+    this.customerService.setDemographicInfo(this.profileForm.value);
+    this.router.navigateByUrl('/dashboard/customers/list-address-info');
   }
 }
--- src/app/features/customers/services/customer/customers.service.ts
+++ src/app/features/customers/services/customer/customers.service.ts
@@ -1,17 +1,27 @@
-
+import { setDemographicInfo } from './../../store/customerToAdd/customerToAdd.actions';
 import { Customer } from './../../models/customer';
-import { Observable, Subject } from 'rxjs';
+import { map, Observable, Subject } from 'rxjs';
 import { environment } from './../../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchCustomer } from '../../models/searchCustomer';
-
+import { Store } from '@ngrx/store';
+import { CustomerState } from '../../store/customer.reducer';
+import { CustomerDemographicInfo } from '../../models/customerDemographicInfo';
 @Injectable({
   providedIn: 'root',
 })
 export class CustomersService {
+  customerToAddModel$: Observable<Customer> = this.store.select(
+    state => state.customerToAdd
+  );
+
   apiControllerUrl: string = `${environment.apiUrl}/customers`;
-  constructor(private httpClient: HttpClient) {}
+
+  constructor(
+    private httpClient: HttpClient,
+    private store: Store<CustomerState>
+  ) {}
 
   getList(): Observable<Customer[]> {
     return this.httpClient.get<Customer[]>(this.apiControllerUrl);
@@ -20,58 +30,67 @@ export class CustomersService {
   getListByFilter(searchCustomer: SearchCustomer): Observable<Customer[]> {
     const subject = new Subject<Customer[]>();
     this.httpClient.get<Customer[]>(this.apiControllerUrl).subscribe({
-      next: (response) => {
+      next: response => {
         let filteredCustomers = response;
         if (searchCustomer.id) {
           filteredCustomers = filteredCustomers.filter(
-            (item) => item.id == searchCustomer.id
+            item => item.id == searchCustomer.id
           );
         }
         if (searchCustomer.customerId) {
           filteredCustomers = filteredCustomers.filter(
-            (item) => item.customerId == searchCustomer.customerId
+            item => item.customerId == searchCustomer.customerId
           );
         }
         if (searchCustomer.accountNumber) {
-          filteredCustomers = filteredCustomers.filter((item) =>
+          filteredCustomers = filteredCustomers.filter(item =>
             item.billingAccounts!.find(
-              (ba) => ba.accountNumber == searchCustomer.accountNumber
+              ba => ba.accountNumber == searchCustomer.accountNumber
             )
           );
         }
 
         if (searchCustomer.gsmNumber) {
           filteredCustomers = filteredCustomers.filter(
-            (item) => item.contactMedium!.mobilePhone == searchCustomer.gsmNumber
+            item => item.contactMedium!.mobilePhone == searchCustomer.gsmNumber
           );
         }
 
         if (searchCustomer.firstName) {
-          filteredCustomers = filteredCustomers.filter(
-            (item) => item.firstName!.toLowerCase().includes(searchCustomer.firstName.toLowerCase())
+          filteredCustomers = filteredCustomers.filter(item =>
+            item
+              .firstName!.toLowerCase()
+              .includes(searchCustomer.firstName.toLowerCase())
           );
         }
         if (searchCustomer.lastname) {
-          filteredCustomers = filteredCustomers.filter(
-            (item) => item.lastName!.toLowerCase().includes(searchCustomer.lastname.toLowerCase())
+          filteredCustomers = filteredCustomers.filter(item =>
+            item
+              .lastName!.toLowerCase()
+              .includes(searchCustomer.lastname.toLowerCase())
           );
         }
         if (searchCustomer.orderNumber) {
-          filteredCustomers = filteredCustomers.filter((item) =>
-            item.billingAccounts!.find((ba) =>
-              ba.orders.find((o) => o.id == searchCustomer.orderNumber)
+          filteredCustomers = filteredCustomers.filter(item =>
+            item.billingAccounts!.find(ba =>
+              ba.orders.find(o => o.id == searchCustomer.orderNumber)
             )
           );
         }
-        subject.next(filteredCustomers)
+        subject.next(filteredCustomers);
       },
-      error:(err)=>{
-        subject.error(err)
+      error: err => {
+        subject.error(err);
       },
-      complete:()=>{        //en son calısan yer
-        subject.complete()
-      }
+      complete: () => {
+        //en son calısan yer
+        subject.complete();
+      },
     });
-    return subject.asObservable()
+    return subject.asObservable();
+  }
+
+  setDemographicInfo(props: CustomerDemographicInfo) {
+    this.store.dispatch(setDemographicInfo(props));
   }
 }
