b425265c554d214297eecded525f89f5c091d774
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2022-09-01 23:23:12 +0300

    contact-medium-list-added
    Co-Authored-By: Çağla Öztürk <ozturkcagla041@gmail.com>
    Co-Authored-By: Sevgi Karaağaç <57255817+sevgikaraagac@users.noreply.github.com>
    Co-Authored-By: Merve Ünver <77447642+MerveUnver@users.noreply.github.com>

--- src/app/features/customers/pages/customer-contact-medium/customer-contact-medium/customer-contact-medium.component.ts
+++ src/app/features/customers/pages/customer-contact-medium/customer-contact-medium/customer-contact-medium.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Customer } from '../../../models/customer';
+import { CustomersService } from '../../../services/customer/customers.service';
 
 @Component({
   templateUrl: './customer-contact-medium.component.html',
@@ -9,9 +12,10 @@ import { ActivatedRoute } from '@angular/router';
 export class CustomerContactMediumComponent implements OnInit {
   selectedCustomerId!:number;
   updateCustomerContactForm!:FormGroup;
+  customer! : Customer
 
   constructor(private formBuilder:FormBuilder,
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute, private messageService: MessageService, private customersService: CustomersService) { }
 
 
   ngOnInit(): void {
@@ -27,10 +31,19 @@ export class CustomerContactMediumComponent implements OnInit {
       faxNumber: ['', Validators.required]
     });
   }
-  getCustomerById() {
+  getCustomerById(){
     this.activatedRoute.params.subscribe(params => {
       if(params['id']) this.selectedCustomerId = params['id'];
+      console.log(this.selectedCustomerId)
     })
+    if(this.selectedCustomerId == undefined) {
+      this.messageService.add({detail: 'Customer not found!...', severity:'danger', summary:'error', key:'etiya-custom'})
+    }
+    else {
+      this.customersService.getCustomerById(this.selectedCustomerId).subscribe(data => {
+        this.customer = data;
+      })
+    }
   }
 
 }
